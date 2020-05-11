2bd8a14183e96e29567b63824197191d31c3d580
Bree Warren <ambreea.warren@gmail.com>
2020-05-11 18:55:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,20 +3,8 @@ def square_array(array)
   new_array = []
   count = 0
   while count < array.length do
-    new_array << {array[count] ** 2}
+    new_array.push(array[count] ** 2)
     count += 1
   end
   new_array
-end
-
-
-array = [1,2,3,4]
-def square_array(array)
-  new_array = []
-  count = 0
-  while count < array.length do
- squared = new_array.push(array[count]**2)
-    count += 1
-  end
-Return new_array
 end
